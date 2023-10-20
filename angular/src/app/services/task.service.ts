5977b0c48e2bfd9a8c965187b299702c931b7b5d
Joshua Weil <joshua.weil@lexisnexis.com>
2023-10-20 09:08:54 -0400

    Added Task Model and Service

---
+++ angular/src/app/services/task.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from '../models/task.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  tasks: Array<Task> = [];
+
+  constructor() {
+    // load the tasks when the service is created
+    this.getTasks().subscribe((tasks: Array<Task>) => {
+      this.tasks = tasks;
+    });
+  }
+
+  private getTasks(): Observable<Array<Task>> {
+    // Normally we would make a call to a back end service using restful http calls
+    // for this demo we emulate that using the of() method from rxjs
+    const mockTasksResponse: Array<Task> = [
+      { id: 1, title: 'Task 1', completed: false },
+      { id: 2, title: 'Task 2', completed: false },
+      { id: 3, title: 'Task 3', completed: false }
+    ];
+    return of(mockTasksResponse);
+  }
+
+  addTask(task: Task): void {
+    this.tasks.push(task);
+  }
+
+  deleteTaskById(id: number): void {
+    this.tasks = this.tasks.filter((task: Task) => task.id !== id);
+  }
+
+}
